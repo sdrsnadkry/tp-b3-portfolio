69da82d216aff75f3349d514df92e505ce9ffbed
Sudarshan Adhikari <sdadkry95@gmail.com>
2024-08-06 13:59:11 +0545

    data types and conversion

---
+++ index.js
@@ -0,0 +1,89 @@
+//Data types
+
+// 1. Number
+
+//keywords
+// const
+
+// keyword variableName operator value
+
+//const is always constant
+const a = 10;
+
+//re declare
+// const a = 20;
+
+// a = 20;
+
+const b = 20;
+
+const sum = a + b;
+
+// console.log(a, b, sum);
+
+// let
+
+let x = 20;
+x = 40;
+
+let y = 40;
+
+// let y= 40;
+let sumXY = x + y;
+// console.log(sumXY);
+
+// var
+
+var a1 = 1;
+
+a1 = 3;
+
+var a2 = 2;
+
+var a2 = 1000;
+
+var sumA1A2 = a1 + a2;
+
+console.log(sumA1A2);
+
+// odd even
+
+const reminder = a1 % 2;
+
+// 2. String
+
+const name1 = "John";
+
+const name2 = "Alice";
+
+const nameSum = name1 + name2;
+
+const random1 = 1;
+
+const random2 = "1";
+
+const random3 = 3;
+
+//type conversion
+// console.log(String(Number(random2)) + random3 + random1);
+// console.log(nameSum)
+
+// 3. Boolean
+
+const isMale = true;
+
+const value = null
+
+console.log(typeof value)
+
+
+//falsy
+const apple = undefined
+const apple2 = null
+
+
+
+
+// 4. Object
+
+// 5. Array
